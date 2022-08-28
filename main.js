bd3fc4946e3a51557057ff881a309372213ef274
Cristopher Mercado <68767452+CristopherAA@users.noreply.github.com>
2022-08-28 00:53:34 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+selectableShapes = document.querySelectorAll('.selectable');
+
+selectableShapes.forEach(element => {
+    element.addEventListener('click',()=>{
+        document.querySelector('.shapes').innerHTML +=
+        `
+        <div class="circle shape draggable tap-target">
+                <p>Soy un circulo</p>
+        </div>
+        `;
+    })
+});
+
+
+interact('.draggable')
+  .draggable({
+    // enable inertial throwing
+    inertia: true,
+    // keep the element within the area of it's parent
+    modifiers: [
+      interact.modifiers.restrictRect({
+        restriction: 'parent',
+        endOnly: true
+      })
+    ],
+    // enable autoScroll
+    autoScroll: true,
+
+    listeners: {
+      // call this function on every dragmove event
+      move: dragMoveListener,
+
+      // call this function on every dragend event
+      end (event) {
+
+      }
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+
+interact('.tap-target')
+  .on('tap', function (event) {
+    event.currentTarget.style.color = "blue";
+    event.preventDefault()
+  })
+  .on('doubletap', function (event) {
+    event.currentTarget.classList.toggle('large')
+    event.currentTarget.classList.remove('rotate')
+    event.preventDefault()
+  })
+  .on('hold', function (event) {
+    event.currentTarget.classList.toggle('rotate')
+    event.currentTarget.classList.remove('large')
+  })
